60ad1a6abbc332b4eba5c5c82d47ed4244af9a1c
Wouter Kayser <wouterkayser@gmail.com>
2019-10-16 13:39:06 +0200

    overlap toevoegen, zo ver als dat werkt

--- beun.m
+++ beun.m
@@ -4,19 +4,21 @@ load('data_assignment.mat');
 signal = EEG(12,:);
 
 segmentSize = 750;
+stepSize = 500;
 orderFilter = 1;
 
-numSegments = ceil(length(signal)/segmentSize);
-signal(segmentSize*numSegments) = 0;
+numSegments = ceil((length(signal)-segmentSize)/stepSize);
 
-segments = reshape(signal, segmentSize, numSegments);
+% segments = reshape(signal, segmentSize, numSegments);
 
-sys = ar(signal(10000:17500), orderFilter, 'yw', 'Ts', 1/250);
+sys = ar(signal(13500:14000), orderFilter, 'yw', 'Ts', 1/250);
 
 power(numSegments) = 0;
 
 for t = 1:numSegments
-    power(t) = norm(filter(sys.A, 1, segments(:, t)));
+    start = 1 + (t-1) * stepSize;
+    final = segmentSize + (t-1) * stepSize;
+    power(t) = norm(filter(sys.A, 1, signal(start:final)));
 end
 
 hold off;
@@ -24,9 +26,11 @@ plot(linspace(0, length(signal)/250, numSegments), power, 'LineWidth', 2);
 hold on;
 plot(linspace(0, length(signal)/250, length(signal)), signal)
 
+
+% 
 [x, I] = max(diff(power));
-xline((I-1) * segmentSize / 250, 'LineWidth', 3, 'Color', 'green');
+xline((segmentSize/2 + I * stepSize)/250, 'LineWidth', 3, 'Color', 'green');
 
 
 title('Captured seizure characteristic');
-legend('Output power of AR model', 'Original signal', 'Estimated start');
+legend('Output power of AR model', 'Original signal');
