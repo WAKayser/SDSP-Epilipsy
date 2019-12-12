ae49d759b9886403623e39200a0c281ddd3dcc8a
Wouter Kayser <wouterkayser@gmail.com>
2019-12-12 12:03:09 +0100

    Update to windowsizes

--- applyMethods.m
+++ applyMethods.m
@@ -5,7 +5,7 @@
 
 % Load the required data
 clear; close all;
-load('data_assignment.mat'); signal = EEG(12,:);
+load('data_assignment.mat'); signal = EEG(15,:);
 
 % Parameters used for the experiments
 segmentSize = 500;
@@ -15,10 +15,10 @@ method = 'ls';
 numSegments = ceil((length(signal)-segmentSize)/stepSize);
 
 % Generation of all filter for the first two methods. 
-sys1 = ar(signal(15000:15250), orderFilter, method, 'Ts', 1/250);
-sys2 = ar(signal(1:250), orderFilter, method, 'Ts', 1/250);
+sys1 = ar(signal(15000:15500), orderFilter, method, 'Ts', 1/250);
+sys2 = ar(signal(1:500), orderFilter, method, 'Ts', 1/250);
 filtered = filter(sys2.A, 1, signal);
-sys3 = ar(filtered(15000:15250), orderFilter, method, 'Ts', 1/250);
+sys3 = ar(filtered(15000:15500), orderFilter, method, 'Ts', 1/250);
 
 
 for t = 1:numSegments
@@ -27,10 +27,10 @@ for t = 1:numSegments
     final = segmentSize + (t-1) * stepSize;
     
     % Finding the power in the segment after applying the first filter
-    power1(t) = norm(filter(1, sys1.A, signal(start:final)))/ 250;
+    power1(t) = norm(filter(1, sys1.A, signal(start:final)))^2;
     % Findign the power in the segment after applying the filter on the
     % whitened signal
-    power2(t) = norm(filter(sys2.A, sys3.A, filtered(start:final)));
+    power2(t) = norm(filter(sys2.A, sys3.A, filtered(start:final)))^2;
     % Finding the AR model that describes that segment
     systemp = ar(signal(start:final), orderFilter, 'yw', 'Ts', 1/250);
     % Multiplying the ability to fit the signal with the required noise to
@@ -40,37 +40,57 @@ end
 
 % Plot approach one
 subplot(311)
-plot(linspace(0, length(signal)/250, numSegments), power1, 'LineWidth', 2);
+yyaxis left;
+plot(linspace(0, length(signal)/250, numSegments), power1 / 10^6, 'LineWidth', 2);
+% ylim([-5*10^3 5*10^3]) % Channel 12
+ylim([-1000 1000]) % channel 15
 hold on;
+ylabel('Power (mV^2)')
+yyaxis right;
 plot(linspace(0, length(signal)/250, length(signal)), signal)
 ylabel('Amplitude (μV)')
 xlabel('Time (s)')
 title('Single AR Filter results')
 set(gca,'FontSize',16)
-ylim([-100 200])
-yline(85)
-legend('Original Signal', 'Result of approach', 'Detector')
+ylim([-150 150])
+yyaxis left;
+% yline(1500, 'LineWidth', 2) %Channel 12: 1000
+yline(250, 'LineWidth', 2) % channel 15
+legend('Result of Approach', 'Detector', 'Original Signal')
 
 %Plot Approach 2
 subplot(312)
+yyaxis left;
 plot(linspace(0, length(signal)/250, numSegments), power2, 'LineWidth', 2);
 hold on;
+ylabel('Power (μV^2)')
+% ylim([-15000 15000]) % channel 12
+ylim([-250000 250000]) % channel 15
+yyaxis right;
 plot(linspace(0, length(signal)/250, length(signal)), signal)
 ylabel('Amplitude (μV)')
 xlabel('Time (s)')
 title('Whitened AR Filter results')
 set(gca,'FontSize',16)
-ylim([-75 150])
-yline(82)
+ylim([-150 150])
+yyaxis left;
+% yline(5500, 'LineWidth', 2) %Channel 12
+yline(50000, 'LineWidth', 2) % channel 15
 
 % Plot Approach 3
 subplot(313)
-plot(linspace(0, length(signal)/250, numSegments), power3 / 10, 'LineWidth', 2);
+yyaxis left;
+plot(linspace(0, length(signal)/250, numSegments), power3, 'LineWidth', 2);
+% ylim([-1800 1800]) % channel 12
+ylim([-3000 3000]) % channel 15
 hold on;
+ylabel('Power (μV^2)')
+yyaxis right;
 plot(linspace(0, length(signal)/250, length(signal)), signal)
 ylabel('Amplitude (μV)')
 xlabel('Time (s)')
 title('Model Fitting approach')
 set(gca,'FontSize',16)
-ylim([-100 150])
-yline(50)
+ylim([-150 150])
+yyaxis left;
+yline(400, 'LineWidth', 2) %channel 12: 400
--- autoCorr.m
+++ autoCorr.m
@@ -8,36 +8,36 @@ clear; close all;
 load('data_assignment.mat');
 signal = EEG(12,:);
 
-before(20, 499) = 0;
-during(20, 499) = 0;
+before(10, 999) = 0;
+during(10, 999) = 0;
 
 % Calculating the autocorrelation at different intervals
-for i = 1:20
-   before(i, :) = xcorr(signal(1+(i-1)*250:i*250));
-   during(i, :) = xcorr(signal(12501+(i-1)*250:12500+i*250)); 
+for i = 1:10
+   before(i, :) = xcorr(signal(1+(i-1)*500:i*500));
+   during(i, :) = xcorr(signal(12501+(i-1)*500:12500+i*500)); 
 end
 
-bias = xcorr(ones(250, 1));
+bias = xcorr(ones(500, 1));
 
 
 % Plotting before seizure behaviour
 subplot(211)
-plot(linspace(-125, 125, 499), (before)./bias', 'color', 'blue');
+plot(linspace(-250, 250, 999), (before)./bias', 'color', 'blue');
 title('Autocorrelation before a seizure')
 ylabel('correlation (μV^2)');
-xlim([-125 125])
+xlim([-250 250])
 xlabel('lags');
 set(gca,'FontSize',16)
 yline(0);
-xlim([-125 125])
+xlim([-250 250])
 
 % Plotting behaviour during seizure. 
 subplot(212)
-plot(linspace(-125, 125, 499), (during)./bias', 'color', 'red');
+plot(linspace(-250, 250, 999), (during)./bias', 'color', 'red');
 title('Autocorrelation during a seizure');
 ylabel('correlation (μV^2)');
 xlabel('lags');
 set(gca,'FontSize',16)
 yline(0);
-xlim([-125 125])
+xlim([-250 250])
 ylim([-3000 5000])
